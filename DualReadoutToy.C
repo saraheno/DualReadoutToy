ec3a72ea2678272b76ddc09311d6c1bbc56fcea8
Sarah <sarah.eno@cern.ch>
2024-08-18 09:25:53 -0400

    first

--- DualReadoutToy.C
+++ DualReadoutToy.C
@@ -19,29 +19,41 @@ void SCEDraw1_2D (TCanvas* canv, const char* name, TH2F* h1, const char* outfile
 int nshowers=10;
 double h_s=0.9;
 double h_c=0.7;
-
+double nscint=1000;
+double ncer=100;
 
 
 void DualReadoutToy() {
 
   TRandom rrr;
-  TH1F *fff = new TH1F("fff","shower em fraction",100,0.,1.);
-  TH1F *sss = new TH1F("sss","shower scintillation",100,0.,1.);
-  TH1F *ccc = new TH1F("ccc","shower cherenkov",100,0.,1.);
-  TH2F *sscc = new TH2F("sscc","cheren versus scint", 100,0.,1.,100,0.,1.);
+  TH1F *fff = new TH1F("fff","shower em fraction",100,0.,1.1);
+  TH1F *sss = new TH1F("sss","shower scintillation",100,0.,1.1);
+  TH1F *ccc = new TH1F("ccc","shower cherenkov",100,0.,1.1);
+  TH2F *sscc = new TH2F("sscc","cheren versus scint", 100,0.,1.1,100,0.,1.1);
+  TH1F *ddd = new TH1F("ddd","dual readout",100,0.,1.1);
 
   for (int i=0;i<nshowers;i++) {
     // pick a value for fraction EM in the shower
-    double a=-1.;
-    while((a<0)||(a>1) ) {
-      a = rrr.Gaus(0.7,0.3);
+    double FFF=-1.;
+    while((FFF<0)||(FFF>1) ) {
+      FFF = rrr.Gaus(0.7,0.3);
     }
-    fff->Fill(a);
-    double SSS=(1+h_s*(1-a));
+    fff->Fill(FFF);
+    double SSS=(FFF+h_s*(1-FFF));
+    SSS=SSS*(1+rrr.Gaus(0.,1/sqrt(nscint)));
     sss->Fill(SSS);
-    double CCC=(1+h_s*(1-a));
+    double CCC=(FFF+h_s*(1-FFF));
+    CCC=CCC*(1+rrr.Gaus(0.,1/sqrt(ncer)));
     ccc->Fill(CCC);
     sscc->Fill(SSS,CCC);
+    std::cout<<"FFF SSS CCC are "<<FFF<<" "<<SSS<<" "<<CCC<<std::endl;
+
+    double DDD=((1-h_c)*SSS - (1-h_s)*CCC)/(h_s-h_c);
+    ddd->Fill(DDD);
+    std::cout<<"DDD is "<<DDD<<std::endl;
+
+
+
   }
 
 
@@ -53,6 +65,8 @@ void DualReadoutToy() {
   SCEDraw1(c3,"c3",ccc,"junk3.png",0);
   TCanvas* c4;
   SCEDraw1_2D(c4,"c4",sscc,"junk4.png");
+  TCanvas* c5;
+  SCEDraw1(c5,"c5",ddd,"junk5.png",0);
 
 }
 
