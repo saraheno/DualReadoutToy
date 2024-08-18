c1afa455cf9cd39b7c089a370c36ab3481f79abe
Sarah <sarah.eno@cern.ch>
2024-08-18 17:30:46 -0400

    yuck

--- DualReadoutToy.C
+++ DualReadoutToy.C
@@ -18,7 +18,7 @@ void SCEDraw1_2D (TCanvas* canv, const char* name, TH2F* h1, const char* outfile
 
 int nshowers=10000;
 double h_s=0.9;
-double h_c=0.6;
+double h_c=0.3;
 double nscint=10;
 double ncer=10;
 double fmean=0.5;
@@ -33,10 +33,12 @@ void DualReadoutToy() {
   TH1F *ccc = new TH1F("ccc","shower cherenkov",300,0.,2.0);
   TH2F *sscc = new TH2F("sscc","cheren versus scint", 1000,0.,2.0,1000,0.,2.0);
   TH1F *ddd = new TH1F("ddd","dual readout",300,0.,2.0);
-  TH1F *cov = new TH1F("cov","covariance",300,0.,2.0);
+  TH1F *cov = new TH1F("cov","covariance",300,-2.,2.0);
 
 
   double acov=0.;
+  double pmeans=(fmean+(1-fmean)*h_s);
+  double pmeanc=(fmean+(1-fmean)*h_c);
   for (int i=0;i<nshowers;i++) {
     // pick a value for fraction EM in the shower
     double FFF=-1.;
@@ -48,7 +50,8 @@ void DualReadoutToy() {
     double SSS=(FFF+h_s*(1-FFF));
     SSS=SSS*(1+rrr.Gaus(0.,1/sqrt(nscint)));
     sss->Fill(SSS);
-    double CCC=(FFF+h_c*(1-FFF));    CCC=CCC*(1+rrr.Gaus(0.,1/sqrt(ncer)));
+    double CCC=(FFF+h_c*(1-FFF));    
+    CCC=CCC*(1+rrr.Gaus(0.,1/sqrt(ncer)));
     ccc->Fill(CCC);
     sscc->Fill(SSS,CCC);
     //std::cout<<"FFF SSS CCC are "<<FFF<<" "<<SSS<<" "<<CCC<<std::endl;
@@ -57,7 +60,7 @@ void DualReadoutToy() {
     ddd->Fill(DDD);
     //std::cout<<"DDD is "<<DDD<<std::endl;
 
-    cov->Fill((SSS-(fmean-(1-fmean)*h_s))*(CCC-(fmean-(1-fmean)*h_c)));
+    cov->Fill((SSS-pmeans)*(CCC-(fmean-pmeanc)));
     acov+=(SSS-(fmean-(1-fmean)*h_s))*(CCC-(fmean-(1-fmean)*h_c));
   }
   acov=acov/(nshowers-1);
@@ -72,13 +75,13 @@ void DualReadoutToy() {
   std::cout<<"dual res is "<<sigmaD<<std::endl;
 
 
-  std::cout<<" mean scint is "<<sss->GetMean()<<" while predicted is "<<(fmean+(1-fmean)*h_s)<<std::endl;
-  std::cout<<" mean cer is "<<ccc->GetMean()<<" while predicted is "<<(fmean+(1-fmean)*h_c)<<std::endl;
-  double covmean = cov->GetMean();
+  std::cout<<" mean scint is "<<sss->GetMean()<<" while predicted is "<<pmeans<<std::endl;
+  std::cout<<" mean cer is "<<ccc->GetMean()<<" while predicted is "<<pmeanc<<std::endl;
+  //double covmean = cov->GetMean();
   int iii=cov->GetMaximumBin();
-  covmean=cov->GetBinCenter(iii);
+  double covmean=cov->GetBinCenter(iii);
   double precov=h_c*(1-h_s)+h_s*(1-h_c)+(1/sqrt(2))*(1-h_s)*(1-h_c);
-  std::cout<<" mean cov is "<<covmean<<" while predicted is "<<precov<<std::endl;
+  std::cout<<" mean cov is "<<acov<<" while predicted is "<<precov<<std::endl;
   std::cout<<"ratio is "<<covmean/precov<<std::endl;
 
 
